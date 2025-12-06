d1d8a6b8f66e28d00707ae64b5a9091cf653dfdd
chacaleta <Abderrahmanabdourial@gmail.com>
2025-12-06 03:50:32 +0100

    finished the first version of the calculator

--- Script.js
+++ Script.js
@@ -5,7 +5,7 @@ let equal = document.getElementById("equal");
 let three = document.getElementById("three");
 let two = document.getElementById("two");
 let one = document.getElementById("one");
-let six = document.getElementById("five");
+let six = document.getElementById("six");
 let five = document.getElementById("five");
 let four = document.getElementById("four");
 let plus = document.getElementById("plus");
@@ -18,13 +18,183 @@ let devide = document.getElementById("devide");
 let clear = document.getElementById("clear");
 let screen = 0;
 let firstnum = "";
-let operator;
+let operator = "";
 let secondnum = "";
+let step = 1;
 
-
-zero.addEventListener("click", function () {
-    screen = 0;
-    display.innerText = screen;
-    
+zero.addEventListener("click", function(){
+    if (step == 1) {
+        firstnum = firstnum + "0";
+        display.innerText = firstnum;
+    } else {
+        secondnum = secondnum + "0"
+        display.innerText = secondnum;
+    }
+})
+one.addEventListener("click", function(){
+    if (step == 1) {
+        firstnum = firstnum + "1";
+        display.innerText = firstnum;
+    } else {
+        secondnum = secondnum + "1"
+        display.innerText = secondnum;
+    }
+})
+two.addEventListener("click", function(){
+    if (step == 1) {
+        firstnum = firstnum + "2";
+        display.innerText = firstnum;
+    } else {
+        secondnum = secondnum + "2"
+        display.innerText = secondnum;
+    }
+})
+three.addEventListener("click", function(){
+    if (step == 1) {
+        firstnum = firstnum + "3";
+        display.innerText = firstnum;
+    } else {
+        secondnum = secondnum + "3"
+        display.innerText = secondnum;
+    }
+})
+four.addEventListener("click", function(){
+    if (step == 1) {
+        firstnum = firstnum + "4";
+        display.innerText = firstnum;
+    } else {
+        secondnum = secondnum + "4"
+        display.innerText = secondnum;
+    }
+})
+five.addEventListener("click", function(){
+    if (step == 1) {
+        firstnum = firstnum + "5";
+        display.innerText = firstnum;
+    } else {
+        secondnum = secondnum + "5"
+        display.innerText = secondnum;
+    }
+})
+six.addEventListener("click", function(){
+    if (step == 1) {
+        firstnum = firstnum + "6";
+        display.innerText = firstnum;
+    } else {
+        secondnum = secondnum + "6"
+        display.innerText = secondnum;
+    }
+})
+seven.addEventListener("click", function(){
+    if (step == 1) {
+        firstnum = firstnum + "7";
+        display.innerText = firstnum;
+    } else {
+        secondnum = secondnum + "7"
+        display.innerText = secondnum;
+    }
+})
+eight.addEventListener("click", function(){
+    if (step == 1) {
+        firstnum = firstnum + "8";
+        display.innerText = firstnum;
+    } else {
+        secondnum = secondnum + "8"
+        display.innerText = secondnum;
+    }
+})
+nine.addEventListener("click", function(){
+    if (step == 1) {
+        firstnum = firstnum + "9";
+        display.innerText = firstnum;
+    } else {
+        secondnum = secondnum + "9"
+        display.innerText = secondnum;
+    }
+})
+kamat.addEventListener("click", function(){
+    if (step == 1) {
+        firstnum = firstnum + ".";
+        display.innerText = firstnum;
+    } else {
+        secondnum = secondnum + "."
+        display.innerText = secondnum;
+    }
 })
 
+plus.addEventListener("click", function(){
+    step = 2;
+    operator = "+";
+    display.innerText = operator;
+})
+minus.addEventListener("click", function(){
+    step = 2;
+    operator = "-";
+    display.innerText = operator;
+})
+devide.addEventListener("click", function(){
+    step = 2;
+    operator = "/";
+    display.innerText = operator;
+})
+multiple.addEventListener("click", function(){
+    step = 2;
+    operator = "x";
+    display.innerText = operator;
+})
+
+equal.addEventListener("click", function(){
+    switch (operator) {
+        case "+":
+            screen = Number(firstnum) + Number(secondnum);
+            display.innerText = screen;
+            firstnum = "";
+            secondnum = "";
+            operator = "";
+            break;
+        case "-":
+            screen = Number(firstnum) - Number(secondnum);
+            display.innerText = screen;
+            firstnum = "";
+            secondnum = "";
+            operator = "";
+            break;
+        case "x":
+            screen = Number(firstnum) * Number(secondnum);
+            display.innerText = screen;
+            firstnum = "";
+            secondnum = "";
+            operator = "";
+            break;
+        case "/":
+            screen = Number(firstnum) / Number(secondnum);
+            display.innerText = screen;
+            firstnum = "";
+            secondnum = "";
+            operator = "";
+            break;
+        default:
+            break;
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+clear.addEventListener("click", function(){
+    firstnum = "";
+    secondnum = "";
+    operator = "";
+    display.innerText = "";
+})
